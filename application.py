b0c41a4aaa630639f1182c2f9c1ebfee245a0dfe
Hunter Ashworth <ashworth.hunter@gmail.com>
2025-10-30 23:36:22 -0500

    website most recent updates

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return render_template("index.html", name="Hunter Ashworth", title="Product Development & Cloud Operations")
+    return render_template("index.html", name="Hunter Ashworth")
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=8080)
